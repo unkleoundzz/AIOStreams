56886dd045eb5b85fdf0d20b755e53beab6ca0a3
unkleoundzz <3056design@gmail.com>
2025-02-22 11:50:46 +1100

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -144,7 +144,7 @@ export class BaseWrapper {
 
     let response = useProxy
       ? fetch(url, {
-          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
+          
           method: 'GET',
           headers: headers,
           signal: AbortSignal.timeout(this.indexerTimeout),
